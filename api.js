f781a7a3e47f3895173b9a7ac8cc3e057d1f77dc
Mihai Filip <filipmihainicolas03@gmail.com>
2023-04-11 15:15:00 +0300

    Created the api.js file with the fetch data from the openweathermap website

---
+++ api.js
@@ -0,0 +1,39 @@
+
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback  callback
+ */
+
+
+
+const api_key = "a40d9867973ea572fadd64f98ba79f4b"
+
+export const fetchData = function(URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+
+export const url = {
+    currentWeather(lat, lon) {
+     return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric` 
+    },
+    forecast(lat, lon) {
+     return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`    
+    },
+    reverseGeo(lat, lon) {
+     return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * 
+     * @param {string} query Search query e.g.: "Cluj-Napoca"
+     * 
+     *  
+     */
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
