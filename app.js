49d0bb4ca2b7a53108cad165e356f3eeb517fa2a
Mihai Filip <filipmihainicolas03@gmail.com>
2023-04-11 15:39:40 +0300

    Started to work on app.js file. Iadded the event listener on multiple elements and the toggle search for mobile devices

---
+++ app.js
@@ -0,0 +1,25 @@
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+/**
+ * Add event listener on multiple elements
+ * @param {NodeList} elements Elements node array
+ * @param {string} eventType Event type e.g.: "click"
+ * @param {Function} callback
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (const element of elements) element.addEventListener(eventType,
+        callback);
+}
+
+/**
+ * Toggle search in mobile devices
+ * 
+ */
+
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
